0d5b40400bb6baea063bcd90b9c4e2b487ea5aa2
FernandoSilvera <fernando.silvera02@gmail.com>
2022-07-04 15:12:47 +0200

    Project created.

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="container">
+
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter",
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variables";
+@import "../assets/scss/utilities";
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <div class="container">
+
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variables";
+@import "../assets/scss/utilities";
+
+</style>

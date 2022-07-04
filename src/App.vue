c0d4677d0901b5a90c55f3972fb9e2da508904ba
FernandoSilvera <fernando.silvera02@gmail.com>
2022-07-04 17:16:30 +0200

    Header half-through.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
+    <!-- header -->
+    <TheHeader />
 
+    <!-- footer -->
+    <TheFooter />
   </div>
 </template>
 
@@ -22,8 +26,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,26 @@
 <template>
-  <header>
+  <header class="bg-dark-purple">
     <div class="container">
+      <div class="row row-cols-3">
+        <div class="col">
+          <img src="/img/light-logo.png" alt="logo maxcoach">
+        </div>
 
+        <div class="col">
+          <nav>
+            <ul>
+              <li v-for="(navLink, i) in navLinks" :key="i">
+                <a href="#">{{ navLink }}</a>
+                
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div class="col">
+
+        </div>
+      </div>
     </div>
   </header>
 </template>
@@ -9,6 +28,11 @@
 <script>
 export default {
   name: "TheHeader",
+  data() {
+    return {
+      navLinks: ["Home", "Pages", "Courses", "Features", "Blog", "Shop",],
+    }
+  }
 }
 </script>
 
@@ -16,4 +40,13 @@ export default {
 @import "../assets/scss/variables";
 @import "../assets/scss/utilities";
 
+li {
+  display: inline-block;
+  margin-right: 1rem;
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+}
 </style>

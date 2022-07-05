2d860ed1d3543b8473a3209d7231c2bf10543c89
FernandoSilvera <fernando.silvera02@gmail.com>
2022-07-05 18:14:37 +0200

    Courses card style fixed.

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="card my-card mb-3" style="max-width: 540px;">
     <div class="row g-0">
-      <div class="col-md-4 my-div">
-        <img :src="`/img/course-${img}.jpg`" class="img-fluid rounded-start" :alt="title">
+      <div class="col-md-4">
+        <div class="my-div">
+          <img :src="`/img/course-${img}.jpg`" class="img-fluid rounded-start" :alt="title">
+        </div>
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">{{ price }}</h5>
-          <p class="card-text">{{ title }}</p>
-          <p class="card-text"><small class="text-muted">{{ lessons }} Lessons</small></p>
-          <p class="card-text"><small class="text-muted">{{ students }} Students</small></p>
+          <h5 class="card-title text-primary">{{ price }}</h5>
+          <p class="card-text fw-bold">{{ title }}</p>
+          <i class="fa-regular fa-file-lines fa-2xs me-2"></i>
+          <p class="card-text me-3 my-card-text"><small class="text-muted">{{ lessons }} Lessons</small></p>
+          <i class="fa-regular fa-user fa-2xs me-2"></i>
+          <p class="card-text my-card-text"><small class="text-muted">{{ students }} Students</small></p>
         </div>
       </div>
     </div>
@@ -38,7 +42,21 @@ export default {
 }
 
 .my-div {
-    border-radius: 50%;
-    overflow: hidden;
+  border-radius: 50%;
+  overflow: hidden;
+
+  img {
+    aspect-ratio: 1 / 1;
+    object-fit: cover;
   }
+}
+
+.my-card-text {
+  display: inline-block;
+  font-size: 12px;
+}
+
+i {
+  vertical-align: middle;
+}
 </style>
